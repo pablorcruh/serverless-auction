44eb9daa05ab0fb30ff90cb7c2748a4fb719edf4
pablorcruh <pablorcruh@gmail.com>
2024-09-08 23:53:04 -0500

    add basic configuration to put items into dynamodb

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,12 +1,27 @@
+const {v4} = require('uuid')
+const AWS = require('aws-sdk')
+
 exports.createAuction = async (event) => {
-  const title = JSON.parse(event.body)
+  const body = JSON.parse(event.body)
+  const title = body.title
   const now = new Date();
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 
   const auction = {
+    id: v4(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString()
   }
+
+  await dynamodb.put(
+    {
+      TableName: process.env.AUCTION_TABLE_NAME,
+      Item: auction
+    }
+  ).promise()
+
   return {
     statusCode: 201,
     body: JSON.stringify({
